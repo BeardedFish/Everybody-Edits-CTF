3d40ed501a558006d98ffe4335da8c050c72a051
Darian Benam <darianbenam@hotmail.com>
2020-07-15 01:54:25 -0400

    Added documentation to PlayerDatabaseRow.cs file

--- Everybody-Edits-CTF/Core/Database/PlayerDatabaseRow.cs
+++ Everybody-Edits-CTF/Core/Database/PlayerDatabaseRow.cs
@@ -6,13 +6,45 @@ namespace Everybody_Edits_CTF.Core.Database
 {
     public class PlayerDatabaseRow
     {
+        /// <summary>
+        /// The username of the player.
+        /// </summary>
         public string Username;
+
+        /// <summary>
+        /// The total number of times that this player won a Capture the Flag game.
+        /// </summary>
         public int TotalWins;
+
+        /// <summary>
+        /// The total number of times that this player lost a Capture the Flag game.
+        /// </summary>
         public int TotalLosses;
-        public int Coins;
+
+        /// <summary>
+        /// The total number of kills this player has accumulated while playing Capture the Flag.
+        /// </summary>
         public int TotalKills;
+
+        /// <summary>
+        /// The amount of coins this player currently possesses.
+        /// </summary>
+        public int Coins;
+
+        /// <summary>
+        /// States whether this player is a new player or not. A new player is someone who does not exist in the MySql database.
+        /// </summary>
         public bool IsNewPlayer;
 
+        /// <summary>
+        /// Constructor for holding data of a single player which will be saved to a MySql database.
+        /// </summary>
+        /// <param name="username">Refer to <see cref="Username"/> for description.</param>
+        /// <param name="totalWins">Refer to <see cref="TotalWins"/> for description.</param>
+        /// <param name="totalLosses">Refer to <see cref="TotalLosses"/> for description.</param>
+        /// <param name="totalKills">Refer to <see cref="TotalKills"/> for description.</param>
+        /// <param name="coins">Refer to <see cref="Coins"/> for description.</param>
+        /// <param name="isNewPlayer">Refer to <see cref="IsNewPlayer"/> for description.</param>
         public PlayerDatabaseRow(string username, int totalWins, int totalLosses, int totalKills, int coins, bool isNewPlayer)
         {
             Username = username;
