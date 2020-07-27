7183484be5c35265a4d6ca7b9968e631605d1e4f
Darian Benam <darianbenam@hotmail.com>
2020-07-27 15:55:18 -0400

    Changed formatting

--- Everybody-Edits-CTF/Core/Bot/EverybodyEditsMessage.cs
+++ Everybody-Edits-CTF/Core/Bot/EverybodyEditsMessage.cs
@@ -7,47 +7,26 @@ namespace Everybody_Edits_CTF.Core.Bot
     public static class EverybodyEditsMessage
     {
         public const string ChatMessage = "say";
-
         public const string ClearWorld = "clear";
-
         public const string GodModeToggled = "god";
-
         public const string InitBegin = "init";
-
         public const string InitEnd = "init2";
-
         public const string MorphableBlockPlaced = "br";
-
         public const string MusicBlockPlaced = "bs";
-
         public const string NonPlayableCharacterBlockPlaced = "bn";
-
         public const string PlaceBlock = "b";
-
         public const string PlayerJoinedWorld = "add";
-
         public const string PlayerLeftWorld = "left";
-
         public const string PlayerMoved = "m";
-
         public const string PlayerRespawn = "tele";
-
         public const string PlayerTeleported = "teleport";
-
         public const string ReloadWorld = "reset";
-
         public const string SetTitle = "name";
-
         public const string SignBlockPlaced = "ts";
-
         public const string SmileyChanged = "face";
-
         public const string TeamChanged = "team";
-
         public const string WorldEnd = "we";
-
         public const string WorldPortalBlockPlaced = "wp";
-
         public const string WorldStart = "ws";
     }
 }
