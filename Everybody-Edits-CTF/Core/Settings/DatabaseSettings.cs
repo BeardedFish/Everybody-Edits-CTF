86e0de75dccfb964148daf93a183b725e2115211
Darian Benam <darianbenam@hotmail.com>
2020-07-29 20:03:01 -0400

    Updated DatabaseSettings.cs file since the old database was lost
    Apparently that "free" database hosting wasn't free. Now the database is hosted locally.

--- Everybody-Edits-CTF/Core/Settings/DatabaseSettings.cs
+++ Everybody-Edits-CTF/Core/Settings/DatabaseSettings.cs
@@ -9,26 +9,31 @@ namespace Everybody_Edits_CTF.Core.Settings
         /// <summary>
         /// The URL to the MySql database.
         /// </summary>
-        public const string ServerUrl = "";
+        private const string ServerUrl = "127.0.0.1";
 
         /// <summary>
         /// The port to access the MySql database.
         /// </summary>
-        public const ushort DatabasePort = 0;
+        private const ushort Port = 3306;
+
+        /// <summary>
+        /// The name of the database.
+        /// </summary>
+        private const string Name = "everybody_edits_ctf";
 
         /// <summary>
         /// The username for the MySql database.
         /// </summary>
-        public const string DatabaseUsername = "";
+        private const string Username = "root";
 
         /// <summary>
         /// The password for the MySql database.
         /// </summary>
-        public const string DatabasePassword = "";
+        private const string Password = "";
 
         /// <summary>
         /// The connection string to access the MySql database via <see cref="MySql.Data.MySqlClient.MySqlConnection"/>.
         /// </summary>
-        public static readonly string SqlConnectionString = $"Server={ServerUrl}; Port={DatabasePort}; Database={DatabaseUsername}; Uid={DatabaseUsername}; Pwd={DataabasePassword};";
+        public static readonly string SqlConnectionString = $"Server={ServerUrl}; Port={Port}; Database={Name}; Uid={Username}; Pwd={Password};";
     }
 }
