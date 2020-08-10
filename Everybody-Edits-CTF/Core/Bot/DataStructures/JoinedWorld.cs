b87c31685015e0f2985a135362bcf8f9047d9eb3
Darian Benam <darianbenam@hotmail.com>
2020-08-09 22:42:05 -0400

    Converted public fields to properties

--- Everybody-Edits-CTF/Core/Bot/DataStructures/JoinedWorld.cs
+++ Everybody-Edits-CTF/Core/Bot/DataStructures/JoinedWorld.cs
@@ -13,17 +13,17 @@ namespace Everybody_Edits_CTF.Core.Bot.DataStructures
         /// <summary>
         /// The width of the Everybody Edits world that the bot joined.
         /// </summary>
-        public static int Width;
+        public static int Width { get; set; }
 
         /// <summary>
         /// The height of the Everybody Edits world that the bot joined.
         /// </summary>
-        public static int Height;
+        public static int Height { get; set; }
 
         /// <summary>
         /// The array of blocks in the Everybody Edits world. This array will have a length of <see cref="Width"/> and a width of <see cref="Height"/>.
         /// </summary>
-        public static Block[,,] Blocks;
+        public static Block[,,] Blocks { get; set; }
 
         /// <summary>
         /// Contains all the players currently in the Everybody Edits world. The integer is the Player id while the Player is the object that contains data about the
