d2622e3cda1412463e97fc1ea2797ee22cbcd6d8
Darian Benam <darianbenam@hotmail.com>
2020-08-14 11:36:09 -0400

    Finished documentation in the EditRightsChangedHandler.cs file

--- Everybody-Edits-CTF/Core/Bot/EventHandlers/Protocol/EditRightsChangedHandler.cs
+++ Everybody-Edits-CTF/Core/Bot/EventHandlers/Protocol/EditRightsChangedHandler.cs
@@ -9,15 +9,21 @@ namespace Everybody_Edits_CTF.Core.Bot.EventHandlers.Protocol
 {
     public sealed class EditRightsChangedHandler : BotEvent
     {
+        /// <summary>
+        /// Event handler for when a when a player gains/loses their edit rights in the Everybody Edits world. This class modified the <see cref="JoinedWorld.Players"/>
+        /// objects.
+        /// </summary>
         public EditRightsChangedHandler() : base(new string[] { EverybodyEditsMessage.EditRightsChanged }, null)
         {
 
         }
 
+        /// <summary>
+        /// Handles when a player gains/loses their edit rights in the Everybody Edits world.
+        /// </summary>
+        /// <param name="message">The message to be handled. This message MUST match the one(s) defined in <see cref="BotEvent.TriggerMessages"/>. If not matched, runtime errors can appear.</param>
         public override void Handle(Message message)
         {
-            base.Handle(message);
-
             int playerId = message.GetInt(0);
 
             if (JoinedWorld.Players.ContainsKey(playerId))
