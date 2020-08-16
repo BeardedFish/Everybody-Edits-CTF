b05bb80f26b4532644e164f2a89860bb31f58efd
Darian Benam <darianbenam@hotmail.com>
2020-08-15 23:57:41 -0400

    Added documentation comments to event argument classes

--- Everybody-Edits-CTF/Core/Bot/EventArgs/EffectToggledEventArgs.cs
+++ Everybody-Edits-CTF/Core/Bot/EventArgs/EffectToggledEventArgs.cs
@@ -9,17 +9,32 @@ namespace Everybody_Edits_CTF.Core.Bot.EventArgs
 {
     public class EffectToggledEventArgs
     {
+        /// <summary>
+        /// The player who received/lost the effect.
+        /// </summary>
         public Player Player { get; set; }
 
+        /// <summary>
+        /// The effect the player received/lost.
+        /// </summary>
         public Effect Effect { get; set; }
 
+        /// <summary>
+        /// States whether the effect was enabled for the player or not.
+        /// </summary>
         public bool IsEffectEnabled { get; set; }
 
-        public EffectToggledEventArgs(Player player, Effect effect, bool isEnabled)
+        /// <summary>
+        /// Constructor for a <see cref="EffectToggledEventArgs"/> object which stores data about when a player receives/loses an effect in Everybody Edits.
+        /// </summary>
+        /// <param name="player">Refer to <see cref="Player"/> for description.</param>
+        /// <param name="effect">Refer to <see cref="Effect"/> for description.</param>
+        /// <param name="isEffectEnabled">Refer to <see cref="IsEffectEnabled"/> for description.</param>
+        public EffectToggledEventArgs(Player player, Effect effect, bool isEffectEnabled)
         {
             Player = player;
             Effect = effect;
-            IsEffectEnabled = isEnabled;
+            IsEffectEnabled = isEffectEnabled;
         }
     }
 }
--- Everybody-Edits-CTF/Core/Bot/EventArgs/PlayerResetEventArgs.cs
+++ Everybody-Edits-CTF/Core/Bot/EventArgs/PlayerResetEventArgs.cs
@@ -9,10 +9,21 @@ namespace Everybody_Edits_CTF.Core.Bot.EventArgs
 {
     public class PlayerResetEventArgs
     {
+        /// <summary>
+        /// States whether the properties of the player (or players) was/were reset or not.
+        /// </summary>
         public bool PropertiesReset { get; set; }
 
+        /// <summary>
+        /// A list of players who were reset.
+        /// </summary>
         public List<Player> PlayersReset { get; set; }
 
+        /// <summary>
+        /// Constructor for a <see cref="PlayerResetEventArgs"/> object which stores data about when a player (or players) is/are reset in Everybody Edits.
+        /// </summary>
+        /// <param name="propertiesReset">Refer to <see cref="PropertiesReset"/> for description.</param>
+        /// <param name="playersReset">Refer to <see cref="PlayersReset"/> for description.</param>
         public PlayerResetEventArgs(bool propertiesReset, List<Player> playersReset)
         {
             PropertiesReset = propertiesReset;
