a8f7d489f5904203abc7f8844617349b01dd6328
Darian Benam <sonic8662@gmail.com>
2020-08-25 08:29:56 -0400

    Changed if statements to switch cases

--- Everybody-Edits-CTF/Program.cs
+++ Everybody-Edits-CTF/Program.cs
@@ -30,109 +30,119 @@ namespace Everybody_Edits_CTF
                                         "=+=...............................................................................................=+=";
 
         /// <summary>
-        /// The case which commands will be compared to.
-        /// </summary>
-        private const StringComparison CommandCompareCase = StringComparison.OrdinalIgnoreCase;
-
-        /// <summary>
         /// Main entry point of the program.
         /// </summary>
         static void Main()
         {
-            CaptureTheFlagBot ctfBot = new CaptureTheFlagBot();
-            ConnectionInformation botConnectionInfo = null;
-
             Console.Title = ProgramTitle;
             Console.WriteLine($"{TitleArt}\n");
 
             Console.WriteLine("Type \"help\" for a list of commands.\n");
 
+            CaptureTheFlagBot ctfBot = new CaptureTheFlagBot();
+            ConnectionInformation botConnectionInfo = null;
+
+            bool quitFlag;
             string userInput;
+
             do
             {
                 Console.Write(">> ");
                 userInput = Console.ReadLine().Trim();
 
-                if (!string.Equals(userInput, "quit", CommandCompareCase))
+                quitFlag = string.Equals(userInput, "quit", StringComparison.OrdinalIgnoreCase);
+
+                if (!quitFlag)
                 {
-                    if (string.Equals(userInput, "cls", CommandCompareCase) || string.Equals(userInput, "clear", CommandCompareCase))
+                    switch (userInput.ToLower())
                     {
-                        Console.Clear();
-                        Console.WriteLine($"{TitleArt}");
-                    }
-                    else if (string.Equals(userInput, "connect", CommandCompareCase))
-                    {
-                        if (botConnectionInfo == null)
-                        {
-                            Console.WriteLine("Can't connect because the bot connection information has not been setup! Type \"setup\" to setup the bot connection information.");
-                        }
-                        else
-                        {
-                            if (!ctfBot.Connected)
+                        case "cls":
+                        case "clear":
                             {
-                                Console.Write("Loading players table from MySql database... ");
-                                PlayersTable.Load();
-                                Console.WriteLine($"[{(PlayersTable.Loaded ? "SUCCESS" : $"FAIL")}]");
-
-                                Console.Write("Connecting to Everybody Edits... ");
-                                Console.WriteLine($"[{(ctfBot.Connect(botConnectionInfo) == null ? "SUCCESS" : $"FAIL")}]");
+                                Console.Clear();
+                                Console.WriteLine($"{TitleArt}");
                             }
-                            else
+                            break;
+                        case "connect":
                             {
-                                Console.WriteLine("The bot is already connected!");
+                                if (botConnectionInfo == null)
+                                {
+                                    Console.WriteLine("Can't connect because the bot connection information has not been setup! Type \"setup\" to setup the bot connection information.");
+                                }
+                                else
+                                {
+                                    if (!ctfBot.Connected)
+                                    {
+                                        Console.Write("Loading players table from MySql database... ");
+                                        PlayersTable.Load();
+                                        Console.WriteLine($"[{(PlayersTable.Loaded ? "SUCCESS" : $"FAIL")}]");
+
+                                        Console.Write("Connecting to Everybody Edits... ");
+                                        Console.WriteLine($"[{(ctfBot.Connect(botConnectionInfo) == null ? "SUCCESS" : $"FAIL")}]");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("The bot is already connected!");
+                                    }
+                                }
                             }
-                        }
-                    }
-                    else if (string.Equals(userInput, "disconnect", CommandCompareCase))
-                    {
-                        if (ctfBot.Connected)
-                        {
-                            Console.WriteLine("Disconnecting...");
+                            break;
+                        case "disconnect":
+                            {
+                                if (ctfBot.Connected)
+                                {
+                                    Console.WriteLine("Disconnecting...");
 
-                            ctfBot.Disconnect();
-                        }
-                        else
-                        {
-                            Console.WriteLine("The bot is not connected!");
-                        }
-                    }
-                    else if (string.Equals(userInput, "help", CommandCompareCase))
-                    {
-                        Console.WriteLine("about - Shows information about the program.");
-                        Console.WriteLine("connect - Connects the bot to Everybody Edits.");
-                        Console.WriteLine("disconnect - Disconnects the bot from Everybody Edits.");
-                        Console.WriteLine("help - Shows a list of commands on how to use the program.");
-                        Console.WriteLine("setup - Sets up the details of the Everybody Edits bot.");
-                    }
-                    else if (string.Equals(userInput, "logs", CommandCompareCase))
-                    {
-                        Console.WriteLine(Logger.LogText == string.Empty ? "Log text is empty." : Logger.LogText);
-                    }
-                    else if (string.Equals(userInput, "setup", CommandCompareCase))
-                    {
-                        Console.WriteLine("[!] NOTE: The information entered here is NOT encrypted or stored securely.\n");
+                                    ctfBot.Disconnect();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("The bot is not connected!");
+                                }
+                            }
+                            break;
+                        case "help":
+                            {
+                                Console.WriteLine("about - Shows information about the program.");
+                                Console.WriteLine("connect - Connects the bot to Everybody Edits.");
+                                Console.WriteLine("disconnect - Disconnects the bot from Everybody Edits.");
+                                Console.WriteLine("help - Shows a list of commands on how to use the program.");
+                                Console.WriteLine("setup - Sets up the details of the Everybody Edits bot.");
+                            }
+                            break;
+                        case "logs":
+                            {
+                                Console.WriteLine(Logger.LogText == string.Empty ? "Log text is empty." : Logger.LogText);
+                            }
+                            break;
+                        case "setup":
+                            {
+                                Console.WriteLine("[!] NOTE: The information entered here is NOT encrypted or stored securely.\n");
 
-                        Console.Write("Email: ");
-                        string botEmail = Console.ReadLine();
+                                Console.Write("Email: ");
+                                string botEmail = Console.ReadLine();
 
-                        Console.Write("Password: ");
-                        string botPassword = Console.ReadLine();
+                                Console.Write("Password: ");
+                                string botPassword = Console.ReadLine();
 
-                        Console.Write("World Id: ");
-                        string worldId = Console.ReadLine();
+                                Console.Write("World Id: ");
+                                string worldId = Console.ReadLine();
 
-                        botConnectionInfo = new ConnectionInformation(botEmail, botPassword, worldId);
+                                botConnectionInfo = new ConnectionInformation(botEmail, botPassword, worldId);
 
-                        Console.WriteLine("\nSetup process complete! You may now connect the bot to Everybody Edits by typing \"connect\".");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid command!");
+                                Console.WriteLine("\nSetup process complete! You may now connect the bot to Everybody Edits by typing \"connect\".");
+                            }
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("Invalid command!");
+                            }
+                            break;
                     }
 
                     Console.WriteLine();
                 }
-            } while (!string.Equals(userInput, "quit", CommandCompareCase));
+            } while (!quitFlag);
         }
     }
 }
